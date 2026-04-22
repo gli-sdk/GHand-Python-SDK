fefee76596e88ca81573804140019f943f1c79f4
huangkai <656383702@qq.com>
2026-04-22 19:04:32 +0800

    feat(adaptive_grasp): update __init__.py with V2.0 public exports

--- src/xiaoyao/adaptive_grasp/__init__.py
+++ src/xiaoyao/adaptive_grasp/__init__.py
@@ -1,10 +1,22 @@
 from .states import GraspState
 from .config import AdaptiveGraspConfig
 from .controller import AdaptiveGrasper
+from .tactility import TactileAnalyzer, TactileAnalysis, PerFingerAnalysis
+from .force_planner import ObjectProfile, ObjectProfileRegistry, ForcePlanner, ForceDecision
+from .safety import SafetyMonitor, SafetyStatus, SafetyReport
 
 __all__ = [
     "GraspState",
     "AdaptiveGraspConfig",
     "AdaptiveGrasper",
+    "TactileAnalyzer",
+    "TactileAnalysis",
+    "PerFingerAnalysis",
+    "ObjectProfile",
+    "ObjectProfileRegistry",
+    "ForcePlanner",
+    "ForceDecision",
+    "SafetyMonitor",
+    "SafetyStatus",
+    "SafetyReport",
 ]
-
